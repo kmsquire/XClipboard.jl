7a7b26f9414dc7fedda47c9e2efc37b3da143da1
Kevin Squire <kevin.squire@gmail.com>
2014-01-31 02:18:22 -0800

    Allow request for multiple targets in xclipboard()

--- src/XClipboard.jl
+++ src/XClipboard.jl
@@ -1,34 +1,48 @@
 module XClipboard
 
 import Base.Multimedia.@textmime
-export xclipboard, xclipboard_MIME_targets, decodemime
+export xclipboard, xclipboard_targets, decodemime
 
 include("XLib.jl")
 include("xmime.jl")
 
 # Get a list of clipboard targets, as X11 Atoms
-function xclipboard_targets(timeout::Int=2)
+xclipboard_target_atoms(timeout::Int=2) = 
+    reinterpret(Atom, xclipboard_request(XA_TARGETS, timeout))
 
-    data = xclipboard_request(XA_TARGETS, timeout)
-    return reinterpret(Atom, data)
+# Get a list of targets as MIME types
+xclipboard_targets(targets::Array{Atom}) = 
+    [MIME(XGetAtomName(atom)) for atom in targets]
+xclipboard_targets(timeout::Int=2) = 
+    xclipboard_targets(xclipboard_target_atoms(timeout))
+
+# Get a Dict of clipboard targets as (MIME_type => Atom)[]
+function xclipboard_targets_atoms(timeout::Int=2)
+    atoms = xclipboard_target_atoms(timeout)
+    return Dict(xclipboard_targets(atoms), atoms)
 end
 
-# Get a list of clipboard targets as "MIME" types
-# (Note that most of the returned types are typically not true MIME types)
-xclipboard_MIME_targets(timeout::Int=2) = xclipboard_MIME_targets(xclipboard_targets(timeout))
-xclipboard_MIME_targets(targets::Array{Atom}) = [MIME(XGetAtomName(atom)) for atom in targets]
+# Main xclipboard function
+function xclipboard(mimetypes::Array{MIME}, timeout::Int=2)
 
-function xclipboard{T}(mimetype::MIME{T}, timeout::Int=2)
+    atoms = xclipboard_targets_atoms(timeout)
+    local request_atom, mimetype
+    for mimetype in mimetypes
+        if (request_atom = get(atoms, mimetype, NoneAtom)) != NoneAtom
+            break
+        end
+    end
 
-    atoms = xclipboard_targets(timeout)
-    atom_d = Dict(xclipboard_MIME_targets(atoms), atoms)
-    request_atom = atom_d[mimetype]
+    if request_atom == NoneAtom
+        error("Requested clipboard target(s) not found.")
+    end
 
     data = xclipboard_request(request_atom, timeout)
 
     return decodemime(mimetype, data)
 end
-
-xclipboard() = xclipboard(MIME("UTF8_STRING"))
+xclipboard(mimetype::MIME, timeout::Int=2) = xclipboard([mimetype], timeout)
+xclipboard(mimetype::String...) = xclipboard([MIME(m) for m in mimetype])
+xclipboard() = xclipboard("UTF8_STRING", "STRING", "TEXT")
 
 end # module
